e7202b212ba008fe2586d82c85d3cefce0fd54a2
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-10-17 19:09:55 -0400

    add post user method

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,11 +1,31 @@
 var express = require('express')
 var app = express();
 
+app.use(express.urlencoded());
+
 app.get('/', function(req, res) {
     res.json({
         "success": true,
         "msg": "you have successfully connected to this server"
     })
+
 })
 
+app.post('/user', function(req, res) {
+    const {username, password, access_level} = req.body
+    var user = require('./modules/user')
+    user.createUser(username, password, access_level)
+        .then((resolve)=>{
+            res.json({
+                success: true,
+                resolve
+            })
+        })
+        .catch((error) => {
+                res.json({
+                    success: false,
+                    error
+                })
+            })
+})
 app.listen(8080)
