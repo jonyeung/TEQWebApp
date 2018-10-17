edf826868c2824b65f2b7f3dff1eb144d375f117
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-10-17 17:47:40 -0400

    add json hello world

--- src/backend/index.js
+++ src/backend/index.js
@@ -2,7 +2,10 @@ var express = require('express')
 var app = express();
 
 app.get('/', function(req, res) {
-    res.send("Hello world");
+    res.json({
+        "success": true,
+        "msg": "you have successfully connected to this server"
+    })
 })
 
 app.listen(8080)
