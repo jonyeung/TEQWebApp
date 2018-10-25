jonyeung/TEQWebApp

commit 5686ec0e98ab77d531d284b22b7efdc0c2ccc32e

Created a new dropdown in userlist to allow users to modify the access level, integrated API to update previous access level to the frontend

src/backend/modules/user.js
@@ -60,8 +60,8 @@ module.exports = {
         })
     }
 
-    changeUserAccessLevel: function(username, access_level) {
-        const query = 'update users set access_level = ? where username = ?'
+    changeUserAccessLevel: function(access_level, id) {
+        const query = 'update users set access_level = ? where id = id'
         return new Promise((resolve, reject) => {
             con.query(query, [access_level, username], function(err, result) {
                 console.log(result)

src/frontend/scripts/script.js
@@ -74,8 +74,18 @@ $(document).ready(function() {
 			if(data.success){
 				$(data.result.users).each(function(index){
 					console.log(this.ID + this.username);
+					
 					var row = '<tr><td>'+this.ID+'</td><td>'+this.username+ '</td><td>'+
-					this.currently_logged_in+'</td><td>'+this.access_level+'</td></tr>'
+					this.currently_logged_in+'</td><td>'+this.access_level+'</td></tr>' + generateDropdown(this.ID)
+
+					function generateDropdown(id) {
+						var dropdown = '<select class="changeLevelDropdown" id="'+id+'" name="usertype"><option value=""' + 
+						'disabled selected>Pick a user type from the dropdown list...</option>' + 
+						'<option value="support_agency">Support Agency</option><option value="TEQ_low_level">TEQ Low Level</option>'+
+						'<option value="TEQ_mid_level">TEQ Mid Level</option><option value="TEQ_high_level">TEQ High Level</option>'+
+						'<option value="UTSC_staff">UTSC Project Staff</option></select>'
+					}
+
 					$('#userList tr:last').after(row);
 				})
 			}else{
@@ -84,4 +94,37 @@ $(document).ready(function() {
 		}
 
 	})
+
+
+	$("button#idsaveButton").on("click", function(){
+		$('.changeLevelDropdown').each(function() {
+			if($(this).val != "Pick a user type from the dropdown list...") {
+				var id = $(this).attr('id')
+				var accessLevel = $(this).val();
+				$.ajax({
+					type:"GET",
+					url: "https://c01.mechanus.io/user",
+					data: ({
+						access_level : accessLevel,
+						id: id
+					}),
+					error: function(){
+						alert("User access update error");
+					},
+					dataType:"json",
+					traditional: true,
+					success:function(data,status){
+						console.log(data);
+						console.log(status);
+						if(data.success){
+							alert("Updated user id:" + data.result.id + "to access level:" + data.result.access_level);
+						}else{
+							alert("Could not update user, please select a new access level");
+						}
+					}
+				})
+			}
+		}
+	})
+
 });

src/frontend/userlist.html
@@ -26,6 +26,7 @@
                 <th>Username</th>
                 <th>Currently Logged In</th>
                 <th>Access Level</th>
+                <th>Modify Access Level</th>
             </tr>
             
         </table>

commit e3e3f204785055163b9c8a17ddd324c38d73b08a

fix bug caused in commit 5686ec0e98ab77d531d284b22b7efdc0c2ccc32e

src/backend/modules/user.js
@@ -58,7 +58,7 @@ module.exports = {
                 }
             })
         })
-    }
+    },
 
     changeUserAccessLevel: function(access_level, id) {
         const query = 'update users set access_level = ? where id = id'