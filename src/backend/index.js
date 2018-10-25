5d539062e38b0db01004255d8623ddbdbdcf4d61
Xia Zhongyang <zhongyang.xia@mail.utoronto.ca>
2018-10-25 01:28:27 -0400

    add insertRow method

--- src/backend/index.js
+++ src/backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express();
 const user = require('./modules/user')
+const agency = require('./modules/agency')
 
 app.use(express.urlencoded());
+app.use(express.json());
 
 app.get('/', function(req, res) {
     res.json({
@@ -48,6 +50,23 @@ app.get('/authenticate', function(req, res) {
 
 app.get('/user', function(req, res) {
     user.getUsers()
+        .then((result) => {
+            res.json({
+                success: true,
+                result
+            })
+        })
+        .catch((error) => {
+            res.json({
+                success: false,
+                error
+            })
+        })
+})
+
+app.post('/insertRow', function(req, res) {
+    const {row} = req.body;
+    agency.insertRow(row)
         .then((result) => {
             res.json({
                 success: true,
