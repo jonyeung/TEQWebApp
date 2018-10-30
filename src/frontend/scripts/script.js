f3ddb6b44f64ac3f598bb6c7c34c9c4223ab1261
Senpaistorm <austin.l@qq.com>
2018-10-30 17:07:22 -0400

    Implemented frontend UI for querying data

--- src/frontend/scripts/script.js
+++ src/frontend/scripts/script.js
@@ -213,10 +213,54 @@ $(document).ready(function() {
 
 	loadFilterButtons();
 	var localAgencyData = agencyData;
-	var colomnNameData = Object.values(agencyData);
+	var colomnNameData = Object.keys(agencyData);
+	var selectedFilters = [];
 	console.log(localAgencyData);
 	console.log(colomnNameData);
 
+	$("button[class=filterOptions]").on("click", function(event){
+		$("ol#selectable").empty();
+		var id = event.target.id;
+		var char = id.charAt(id.length - 1);
+		var row = "";
+		for(var i = 0; i < colomnNameData.length; i++){
+			if(colomnNameData[i].charAt(0).toUpperCase() == char){
+				row += '<li><button class="colomnNamesList" id=' + localAgencyData[colomnNameData[i]]+
+				'>' + colomnNameData[i] + '</button></li>';
+			}
+		}
+		$("ol#selectable").append(row);
+	});
+
+    $(function() {
+	    $( "#queryInput" ).autocomplete({
+	       source: colomnNameData
+	    });
+ 	});
+
+ 	$("button#addFilterButton").on("click", function(event){
+ 		var searchContent = $("input#queryInput").val();
+ 		if($.inArray(searchContent, colomnNameData) != -1){
+ 			if($.inArray(searchContent,selectedFilters) == -1){
+ 				selectedFilters.push(searchContent);
+ 				console.log(selectedFilters);
+ 				updateSelectedFilters(selectedFilters);
+ 			}
+ 		}
+ 	});
+
+	$("button#clearFilterButton").on("click",function(event){
+		selectedFilters = [];
+		updateSelectedFilters(selectedFilters);
+	});
+
+	$('ol#selectable').on("click", "button.colomnNamesList",function(event){
+		var selected = event.target;
+		if($.inArray($(selected).text(), selectedFilters) == -1){
+			selectedFilters.push($(selected).text());
+			updateSelectedFilters(selectedFilters);
+		}
+	});
 });
 
 
@@ -270,15 +314,21 @@ function cleanData(data, formType){
 }
 
 function loadFilterButtons(){
-	var alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	var alphabet = 'ABCDEFGHILMNOPRSTUWY';
 	var buttons = '';
 	for (var i = 0; i < alphabet.length; i++) {
 		id = "filter" + alphabet.charAt(i);
   		buttons += '<button class="filterOptions" id='+id+'>'+ alphabet.charAt(i)+'</button>';
 	}
-
 	$("div#filterByLetter").append(buttons);
 }
 
-
+function updateSelectedFilters(filters){
+	var buttons = '';
+	$("div#selectedFilters").empty();
+	for(var i = 0; i < filters.length; i++){
+		buttons += '<button class="selectedFilters">' + filters[i] + '</button>';
+	}
+	$("div#selectedFilters").append(buttons);
+}
 
