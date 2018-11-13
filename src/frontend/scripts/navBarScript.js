c4a5361f89e90513e0c3e54a75ab4dcc4beec031
michelle-pasquill <mimi.191234@hotmail.com>
2018-11-13 18:49:04 -0500

    Added script references to the navBarScript.js in all the html other than index.html.

--- src/frontend/scripts/navBarScript.js
+++ src/frontend/scripts/navBarScript.js
@@ -1,6 +1,20 @@
-$(document).ready(function()) {
+// This runs on every page other than the index.html
+$(document).ready(function() {
+
+  loadHeader();
+
   // Header click redirects to dashboard.
-	$("header").not("#loginHeader").on("click", function() {
+	$("header").on("click", function() {
 		document.location.href = "dashboard.html";
 	});
-}
+
+  // Loads the page header into the header tag.
+  function loadHeader() {
+    var content = "<h1><span id='teqHead'>Toronto East Quadrant</span>" +
+    "<br><span id='lipHead'>Local Immigration Partnership</span></h1>" +
+    "<button id='helpBtn'><img src=''></button>";
+    $("header").empty();
+    $("header").append(content);
+  }
+
+});
