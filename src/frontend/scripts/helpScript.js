201f3c7d6e06567b025716cd925c9bace6f24b9a
michelle-pasquill <mimi.191234@hotmail.com>
2018-11-13 20:06:07 -0500

    Added a script for the help menu that uses the jquery ui.

---
+++ src/frontend/scripts/helpScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  setUpMenu();
+
+  function setUpMenu() {
+    $("#helpMenuOptions").accordion({
+      collapsible: true,
+      active: false; //Can be set to number
+    });
+  }
+});
--- src/frontend/scripts/navBarScript.js
+++ src/frontend/scripts/navBarScript.js
@@ -17,4 +17,7 @@ $(document).ready(function() {
     $("header").append(content);
   }
 
+  function loadHelp() {
+  }
+
 });
